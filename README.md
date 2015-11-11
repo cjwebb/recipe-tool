4f8e43fa97bbca54f5b3e33e1860238d2f9a1024
cjwebb <colinjameswebb@hotmail.com>
2015-11-11 20:08:21 +0000

    Basic Tool Working
    Committed to Github just in case I ever need to use it again

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # recipe-tool
 
 A shoddy web-app to help with categorizing recipes
+
+Before running it, execute init! via the REPL
--- src/recipe_tool/handler.clj
+++ src/recipe_tool/handler.clj
@@ -9,28 +9,37 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 ; ------------- data -----------
+(defmacro wcar* [& body] `(redis/wcar nil ~@body))
+
 (def recipes
   (parsed-seq (clojure.java.io/reader "/kickstand/munchcal/recipe-tool/nice.json")))
 
-(defn get-a-recipe []
-  (first recipes))
+(defn init! 
+  "Read all the recipes and put into a 'todo' set"
+  []
+  (doseq [x recipes]
+    (wcar* (redis/sadd "todo" x))))
 
-(defmacro wcar* [& body] `(redis/wcar nil ~@body))
+(defn get-a-recipe []
+  (wcar* (redis/spop "todo")))
 
 ; ---------- handlers -----------
 (defn show-recipe []
   (let [recipe (clojure.walk/keywordize-keys (get-a-recipe))
+        remaining (wcar* (redis/scard "todo"))
         btn-style "width:100px;height:100px"]
     (html
       [:div
+       [:p remaining]
        [:h1 [:a {:href (:url recipe)} (:name recipe)]]
-       [:p ]
+       [:p (:id recipe)]
        [:p (:description recipe)]
        [:img {:src (:image recipe)}]
        [:p (clojure.string/join ", " (:parsed_ingredients recipe))]
        [:p (clojure.string/join ", " (:ingredients recipe))]]
       [:form {:method "post"}
       (hidden-field :recipe-id (:id recipe))
+      [:button {:type "submit" :name "tag" :value "starter" :style btn-style} "Starter"]
       [:button {:type "submit" :name "tag" :value "main" :style btn-style} "Main Meal"]
       [:button {:type "submit" :name "tag" :value "side" :style btn-style} "Side"]
       [:button {:type "submit" :name "tag" :value "dessert" :style btn-style} "Dessert"]
